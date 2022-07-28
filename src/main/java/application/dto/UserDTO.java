60d7eadc51e040768006ebf08e1fe2c50186e64b
sergio-macedo <sergio.garoma@hotmail.com>
2022-07-28 13:32:34 +0200

    using DTO to return users

---
+++ src/main/java/application/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package application.dto;
+
+import application.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+
+    }
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
